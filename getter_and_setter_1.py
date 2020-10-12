d77706c7d6b72a1d2c87da835cd8def4f252332d
tpuser777 <63839869+tpuser777@users.noreply.github.com>
2020-10-12 11:55:08 +0530

    Create getter_and_setter_1.py

---
+++ getter_and_setter_1.py
@@ -0,0 +1,4 @@
+tesla_model_s = Vehicle(4, 'electric', 5, 250)
+print(tesla_model_s.number_of_wheels) # 4
+tesla_model_s.number_of_wheels = 2 # setting number of wheels to 2
+print(tesla_model_s.number_of_wheels) # 2
