9909df3374cf5b18c8f2f76e935b724b5d658ba7
tpuser777 <63839869+tpuser777@users.noreply.github.com>
2020-10-12 12:03:42 +0530

    Create inheritance_subclass.py

---
+++ inheritance_subclass.py
@@ -0,0 +1,4 @@
+my_electric_car = ElectricCar(4, 5, 250)
+print(my_electric_car.number_of_wheels) # => 4
+print(my_electric_car.seating_capacity) # => 5
+print(my_electric_car.maximum_velocity) # => 250
