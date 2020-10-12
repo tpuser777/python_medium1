64fc4fdf0af8f8501b51805d277e5a645a7391b9
tpuser777 <63839869+tpuser777@users.noreply.github.com>
2020-10-12 11:49:26 +0530

    Create dictionary_iteration_2.py

---
+++ dictionary_iteration_2.py
@@ -0,0 +1,15 @@
+dictionary_tk = {
+  "name": "Leandro",
+  "nickname": "Tk",
+  "nationality": "Brazilian",
+  "age": 24
+}
+
+
+for attribute, value in dictionary_tk.items():
+    print("My %s is %s" %(attribute, value))
+    
+# My name is Leandro
+# My nickname is Tk
+# My nationality is Brazilian
+# My age is 24
