90d333df0219d6b3a85ff32d7b1b633de0044f12
tpuser777 <63839869+tpuser777@users.noreply.github.com>
2020-10-12 12:01:35 +0530

    Create python_inheritance.py

---
+++ python_inheritance.py
@@ -0,0 +1,3 @@
+class ElectricCar(Car):
+    def __init__(self, number_of_wheels, seating_capacity, maximum_velocity):
+        Car.__init__(self, number_of_wheels, seating_capacity, maximum_velocity)
