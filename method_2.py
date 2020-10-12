36b920c8d9371374ceb900218e775dc6db888991
tpuser777 <63839869+tpuser777@users.noreply.github.com>
2020-10-12 11:55:27 +0530

    Create method_2.py

---
+++ method_2.py
@@ -0,0 +1,10 @@
+class Vehicle:
+    def __init__(self, number_of_wheels, type_of_tank, seating_capacity, maximum_velocity):
+        self.number_of_wheels = number_of_wheels
+        self.type_of_tank = type_of_tank
+        self.seating_capacity = seating_capacity
+        self.maximum_velocity = maximum_velocity
+
+
+    def make_noise(self):
+        print('VRUUUUUUUM')
