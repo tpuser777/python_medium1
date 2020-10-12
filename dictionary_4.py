cbdcf2c2c9ac5b26009d57c8462bebda7fab9d89
tpuser777 <63839869+tpuser777@users.noreply.github.com>
2020-10-12 11:47:24 +0530

    Create  dictionary_4.py

---
+++ dictionary_4.py
@@ -0,0 +1,11 @@
+dictionary_tk = {
+  "name": "Leandro",
+  "nickname": "Tk",
+  "nationality": "Brazilian"
+}
+
+
+dictionary_tk['age'] = 24
+
+
+print(dictionary_tk) # {'nationality': 'Brazilian', 'age': 24, 'nickname': 'Tk', 'name': 'Leandro'}
